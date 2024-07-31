ec0b3c0ea06e1c6beb83234ad49d86011cb7cf08
Maks <70328902+fatalcr-git@users.noreply.github.com>
2024-07-31 15:29:01 +0300

    Add files via upload

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -3,22 +3,31 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FactorialTest {
-    @DataProvider(name = "factorialTest")
-    public Object[][] factorialDataProvider() {
-        return new Object[][] {
-                {0, 1},
-                {1, 1},
-                {5, 120}
-        };
+    @DataProvider(name = "factorialPositiveTest")
+    public Object[][] factorialPositiveDataProvider() {
+        return new Object[][]{{0, 1}, {1, 1}, {5, 120}, {19, 121645100408832000L}, {20, 2432902008176640000L}};
     }
 
-    @Test(dataProvider = "factorialTest")
-    public void positiveTestFactorial(int n, int expected) {
-        Assert.assertEquals(expected, Factorial.factorial(n));
+    @DataProvider(name = "factorialNegativeTest")
+    public Object[][] factorialNegativeDataProvider() {
+        return new Object[][]{{-1, IllegalArgumentException.class}, {21, IllegalArgumentException.class}};
+    }
+
+    @Test(dataProvider = "factorialPositiveTest")
+    public void positiveTestFactorial(int n, long expected) {
+        Assert.assertEquals(
+                expected,
+                Factorial.factorial(n),
+                "Factorial of " + n + " is " + expected + " but got " + Factorial.factorial(n));
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void negativeTestFactorial() {
+    public void negativeTestFactorialOfLowerLimit() {
         Factorial.factorial(-1);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void negativeTestFactorialOfUpperLimit() {
+        Factorial.factorial(21);
     }
 }
