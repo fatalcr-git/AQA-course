a665149b0be1efc5f9fc0a44d0685b58714f0602
Maks <70328902+fatalcr-git@users.noreply.github.com>
2024-08-13 15:50:42 +0300

    Add files via upload

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
     public WebDriver driver;
@@ -74,8 +78,10 @@ public class MainPage {
         cookieAgree.click();
     }
 
-    public void clickAboutServiceButton() {
-        aboutServiceButton.click();
+    public String getAboutServiceButton() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(@href, 'oplaty')]")));
+        return aboutServiceButton.getAttribute("href");
     }
 
     public void inputPhone(String phone) {
